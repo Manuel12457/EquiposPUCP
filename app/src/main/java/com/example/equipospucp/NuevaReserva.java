b84441d7f0aa890f5eceb0104cb261a3376ed46d
Manuel <a20190133@pucp.edu.pe>
2022-12-01 02:17:42 -0500

    Layouts/items faltantes creados

---
+++ app/src/main/java/com/example/equipospucp/NuevaReserva.java
@@ -0,0 +1,14 @@
+package com.example.equipospucp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_reserva);
+    }
+}
