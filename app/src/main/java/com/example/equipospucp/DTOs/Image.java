938923086e934223de5044c2c25716de87b24578
Ronny Pastor <a20185534@pucp.edu.pe>
2022-12-05 19:28:26 -0500

    ListaDispositivoAdapter imagen cargada la primera que coincida

---
+++ app/src/main/java/com/example/equipospucp/DTOs/Image.java
@@ -0,0 +1,27 @@
+package com.example.equipospucp.DTOs;
+
+public class Image {
+    private String dispositivo;
+    private String imagen;
+
+    public Image(String dispositivo, String imagen) {
+        this.dispositivo = dispositivo;
+        this.imagen = imagen;
+    }
+
+    public String getDispositivo() {
+        return dispositivo;
+    }
+
+    public void setDispositivo(String dispositivo) {
+        this.dispositivo = dispositivo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
