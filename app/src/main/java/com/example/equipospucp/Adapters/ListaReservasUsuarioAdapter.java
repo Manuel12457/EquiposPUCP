f2c5f5f341c03090b125b329fbb5244756bd3b13
Manuel <a20190133@pucp.edu.pe>
2022-12-11 04:02:52 -0500

    Adapters

--- app/src/main/java/com/example/equipospucp/Adapters/ListaReservasUsuarioAdapter.java
+++ app/src/main/java/com/example/equipospucp/Adapters/ListaReservasUsuarioAdapter.java
@@ -13,14 +13,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.equipospucp.DTOs.Dispositivo;
-import com.example.equipospucp.DTOs.DispositivoDetalleDto;
-import com.example.equipospucp.DTOs.Reserva;
 import com.example.equipospucp.DTOs.ReservaDto;
 import com.example.equipospucp.DTOs.Usuario;
 import com.example.equipospucp.DetallesDispositivo;
 import com.example.equipospucp.DetallesReserva;
 import com.example.equipospucp.R;
-import com.example.equipospucp.databinding.ReservasFragmentBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,12 +25,12 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ListaReservasAdapter extends RecyclerView.Adapter<ListaReservasAdapter.ReservaViewHolder> {
+public class ListaReservasUsuarioAdapter extends RecyclerView.Adapter<ListaReservasUsuarioAdapter.ReservaUsuarioViewHolder>{
 
     private ArrayList<ReservaDto> listaReservas;
     private Context context;
 
-    public ListaReservasAdapter(ArrayList<ReservaDto> listaReservas, Context context) {
+    public ListaReservasUsuarioAdapter(ArrayList<ReservaDto> listaReservas, Context context) {
         this.setListaReservas(listaReservas);
         this.setContext(context);
     }
@@ -56,39 +53,24 @@ public class ListaReservasAdapter extends RecyclerView.Adapter<ListaReservasAdap
 
     @NonNull
     @Override
-    public ReservaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_reservas, parent, false);
-        return new ListaReservasAdapter.ReservaViewHolder(itemView);
+    public ListaReservasUsuarioAdapter.ReservaUsuarioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_reservasusuario, parent, false);
+        return new ListaReservasUsuarioAdapter.ReservaUsuarioViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ReservaViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ListaReservasUsuarioAdapter.ReservaUsuarioViewHolder holder, int position) {
 
         ReservaDto reservaDto = getListaReservas().get(position);
-        //Cambiarlo por imagen en firebase db. Colocar imagen por defecto si no posee
-        FirebaseDatabase.getInstance().getReference("usuarios").child(reservaDto.getReserva().getIdUsuario())
-                        .addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                if (snapshot.exists()) {
-                                    Usuario usuario = snapshot.getValue(Usuario.class);
-                                    holder.textView_usuario.setText(usuario.getCodigo() + "\n" + usuario.getRol());
-                                }
-                            }
 
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
-
+        holder.textView_fechareserva_reservasusuario.setText(reservaDto.getReserva().getFechayhora());
         FirebaseDatabase.getInstance().getReference("dispositivos").child(reservaDto.getReserva().getIddispositivo())
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if (snapshot.exists()) {
                             Dispositivo dispositivo = snapshot.getValue(Dispositivo.class);
-                            holder.textView_fechaydispositivo.setText(reservaDto.getReserva().getFechayhora() + "\nReserva: " + dispositivo.getTipo() + " " + dispositivo.getMarca());
+                            holder.textView_inforeserva_reservausuario.setText("Reserva: " + dispositivo.getTipo() + " " + dispositivo.getMarca() +"\nTiempo de resesrva: " + reservaDto.getReserva().getTiempoReserva() + " días");
                         }
                     }
 
@@ -98,15 +80,14 @@ public class ListaReservasAdapter extends RecyclerView.Adapter<ListaReservasAdap
                     }
                 });
 
-        holder.textView_estado.setText(reservaDto.getReserva().getEstado());
+        holder.textView_estadoReservausuario.setText(reservaDto.getReserva().getEstado());
         if (reservaDto.getReserva().getEstado().equals("APROBADO")) {
-            holder.textView_estado.setTextColor(Color.parseColor("#4CAF50"));
+            holder.textView_estadoReservausuario.setTextColor(Color.parseColor("#4CAF50"));
         } else if (reservaDto.getReserva().getEstado().equals("PENDIENTE")) {
-            holder.textView_estado.setTextColor(Color.parseColor("#7E877E"));
+            holder.textView_estadoReservausuario.setTextColor(Color.parseColor("#7E877E"));
         } else {
-            holder.textView_estado.setTextColor(Color.parseColor("#E45252"));
+            holder.textView_estadoReservausuario.setTextColor(Color.parseColor("#E45252"));
         }
-        //poder seleccionarlo
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,17 +105,17 @@ public class ListaReservasAdapter extends RecyclerView.Adapter<ListaReservasAdap
         return getListaReservas().size();
     }
 
-    public class ReservaViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView_usuario;
-        public TextView textView_fechaydispositivo;
-        public TextView textView_estado;
-        public ImageView imageView;
-        public ReservaViewHolder(@NonNull View itemView) {
+    public class ReservaUsuarioViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView_fechareserva_reservasusuario;
+        public TextView textView_inforeserva_reservausuario;
+        public TextView textView_estadoReservausuario;
+        public ImageView idImagenDispositivoReservas;
+        public ReservaUsuarioViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.textView_usuario = itemView.findViewById(R.id.textView_InfoUsuarioreservas);
-            this.textView_fechaydispositivo = itemView.findViewById(R.id.textView_fechareserva_reservas);
-            this.textView_estado = itemView.findViewById(R.id.textView_estadoReserva);
-            this.imageView = itemView.findViewById(R.id.idImagenUsuarioReservas);
+            this.textView_fechareserva_reservasusuario = itemView.findViewById(R.id.textView_fechareserva_reservasusuario);
+            this.textView_inforeserva_reservausuario = itemView.findViewById(R.id.textView_inforeserva_reservausuario);
+            this.textView_estadoReservausuario = itemView.findViewById(R.id.textView_estadoReservausuario);
+            this.idImagenDispositivoReservas = itemView.findViewById(R.id.idImagenDispositivoReservas);
         }
     }
 }
